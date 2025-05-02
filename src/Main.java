166095f35fe6a1233130dbdb1383fefa1716264b
Jason <28811675+jsonjv@users.noreply.github.com>
2025-05-02 23:51:08 +0800

    feat: implement basic error handling

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,42 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        int principal;
+        while (true) {
+            System.out.print("Principal (1K - 1M): ");
+            principal = scanner.nextInt();
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
 
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
+            System.out.println("Enter a number between 1,000 and 1,000,000");
+        }
+
+        float monthlyInterest;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            float annualInterest = scanner.nextFloat();
+
+            if (annualInterest > 0 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+
+            System.out.println("Enter a value greater than 0 and less than or equal to 30");
+        }
+
+        int numberOfPayments;
+        while (true) {
+            System.out.print("Period (Years): ");
+            byte years = scanner.nextByte();
+
+            if (years > 0 && years <= 30) {
+                numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+
+            System.out.println("Enter a value between 1 and 30");
+        }
 
         double mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
